583471eb0d49ac534c851eacadbcfd5b14985272
Brandon <brandon139@protonmail.com>
2024-07-15 16:02:36 +1000

    ...

--- JsonGreeting.cs
+++ JsonGreeting.cs
@@ -6,3 +6,16 @@ public class JsonGreeting
     public string Name { get; set; }
     public string Content { get; set; }
 }
+
+public class JsonFruit
+{
+    public string Name { get; set; }
+    public string Color { get; set; }
+}
+
+public class JsonFruitBasket
+{
+    public string Owner { get; set; }
+    public List<JsonFruit> Fruits { get; set; }
+    public int TotalWeight { get; set; }
+}
--- Program.cs
+++ Program.cs
@@ -7,23 +7,20 @@ using ProtobufServer;
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.UseStaticFiles();
-
-// Create a file provider for the root directory
-var fileProvider = new PhysicalFileProvider(app.Environment.ContentRootPath);
-
 // Create a content type provider and add the .proto mime type
 var contentTypeProvider = new FileExtensionContentTypeProvider();
 contentTypeProvider.Mappings[".proto"] = "text/plain";
 
+app.UseStaticFiles(); // This serves from wwwroot by default
 
-// Serve files from the root directory, including message.proto
 app.UseStaticFiles(new StaticFileOptions
 {
-    FileProvider = fileProvider,
-    RequestPath = "",
+    FileProvider = new PhysicalFileProvider(
+        Path.Combine(app.Environment.ContentRootPath, "Protos")),
+    RequestPath = "/Protos",
     ContentTypeProvider = contentTypeProvider
 });
+
 
 app.MapPost("/greeting", (HttpRequest request) =>
 {
@@ -51,6 +48,64 @@ app.MapGet("/api/greeting/{name}", (string name) =>
     };
 
     return Results.Bytes(greeting.ToByteArray(), "application/x-protobuf");
+});
+
+app.MapGet("/api/fruits/{owner}", (string owner) =>
+{
+    var fruitBasket = new FruitBasket
+    {
+        Owner = owner,
+        TotalWeight = 500 // grams
+    };
+    var random = new Random();
+    var fruits = new List<(string Name, string Color)>
+{
+    ("Apple", "Red"),
+    ("Banana", "Yellow"),
+    ("Grape", "Purple"),
+    ("Orange", "Orange"),
+    ("Strawberry", "Red"),
+    ("Blueberry", "Blue"),
+    ("Kiwi", "Brown"),
+    ("Mango", "Yellow"),
+    ("Pineapple", "Yellow"),
+    ("Watermelon", "Green"),
+    ("Peach", "Pink"),
+    ("Pear", "Green"),
+    ("Cherry", "Red"),
+    ("Blackberry", "Black"),
+    ("Raspberry", "Red"),
+    ("Lemon", "Yellow"),
+    ("Lime", "Green"),
+    ("Plum", "Purple"),
+    ("Pomegranate", "Red"),
+    ("Apricot", "Orange"),
+    ("Fig", "Purple"),
+    ("Coconut", "Brown"),
+    ("Papaya", "Orange"),
+    ("Guava", "Green"),
+    ("Dragonfruit", "Pink"),
+    ("Passionfruit", "Purple"),
+    ("Lychee", "White"),
+    ("Starfruit", "Yellow"),
+    ("Durian", "Green"),
+    ("Jackfruit", "Green")
+};
+
+    // Randomly select 15-20 fruits for the basket
+    int fruitCount = random.Next(15, 21);
+    var selectedFruits = fruits.OrderBy(x => random.Next()).Take(fruitCount).ToList();
+
+    foreach (var fruit in selectedFruits)
+    {
+        fruitBasket.Fruits.Add(new Fruit { Name = fruit.Name, Color = fruit.Color });
+    }
+
+    // Update total weight based on the number of fruits
+    fruitBasket.TotalWeight = fruitCount * 100; // Assuming an average of 100 grams per fruit
+    var response = new FruitResponse { Basket = fruitBasket };
+
+    return Results.Bytes(response.ToByteArray(), "application/x-protobuf");
 });
 
 // New JSON POST endpoint
